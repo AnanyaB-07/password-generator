092a81bc85e8252405bd7a4a90d2082074ca95a8
AnanyaB-07 <ananya.b20077@gmail.com>
2025-09-30 20:56:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function generatePassword() {
+  const length = 12;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  document.getElementById("passwordOutput").textContent = password;
+}
+
+function copyPassword() {
+  const password = document.getElementById("passwordOutput").textContent;
+  navigator.clipboard.writeText(password).then(() => {
+    alert("Password copied to clipboard!");
+  });
+}
